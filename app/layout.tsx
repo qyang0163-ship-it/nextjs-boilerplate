5eb50d3678fcba80d1154ce80b782772e42f1a98
qyang0163-ship-it <qyang0163@gmail.com>
2026-03-11 14:08:12 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,11 +26,29 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* AdSense */}
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2120779414485850"
           crossOrigin="anonymous"
           strategy="afterInteractive"
+        />
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-4TPNJ8H59P"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-4TPNJ8H59P');
+            `,
+          }}
         />
       </head>
       <body
